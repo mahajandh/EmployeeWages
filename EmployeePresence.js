e4ca75e34817ff62b6f6c58f30f8e99c795e22e6
mahajandh <dhruvmahajan73@gmail.com>
2025-03-20 16:41:24 +0530

    UC8 code pushed

--- EmployeePresence.js
+++ EmployeePresence.js
@@ -145,3 +145,27 @@ function totalDaysWorked(numOfDays, dailyWage) {
 }
 console.log("UC 7G — Number of Days Emp Worked: "+
     empDailyWageArr.reduce(totalDaysWorked ,0));
+    // UC8 Maps
+
+totalEmpHrs = 0;
+totalWorkingDays = 0;
+empDailyWageArr = new Array();
+let empDailyWageMap = new Map();
+function calcDailyWage(empHrs){
+    return empHrs * WAGE_PER_HOUR;
+}
+while(totalEmpHrs <=MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS){
+    totalWorkingDays++;
+    let empCheck = Math. floor (Math. random() * 10)%3;
+    let empHrs = getWorkingHours(empCheck);
+    totalEmpHrs +=empHrs;
+    empDailyWageArr.push(calcDailyWage(empHrs));
+    empDailyWageMap.set(totalWorkingDays, calcDailyWage(empHrs));
+}
+console.log("UC8- Emp Daily Wage map:")
+console.log (empDailyWageMap);
+function totalWages(totalWage, dailyWage) {
+    return totalWage + dailyWage;
+}
+console.log("UC8 — Emp Wage Map totalHrs: " +
+            Array.from(empDailyWageMap.values()).reduce(totalWages ,0));
