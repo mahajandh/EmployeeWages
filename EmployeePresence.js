fe0e6cb39b551cc1fb5758c88c977e95535d50f7
mahajandh <dhruvmahajan73@gmail.com>
2025-03-20 22:03:46 +0530

    UC10 codes pushed

--- EmployeePresence.js
+++ EmployeePresence.js
@@ -120,3 +120,23 @@ let totalSalary = empDailyWageArr.filter(dailyWage => dailyWage > 0).reduce(find
     console.log("Full Working Days: "+fullWorkingDays);
     console.log("Part Working Days: "+partWorkingDays);
     console.log("Non Working Days: "+nonWorkingDays);
+    //UC10
+totalEmpHrs =0;
+totalWorkingDays =0;
+let empDailyHrsAndWageArr = new Array();
+while(totalEmpHrs <=MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS){
+    totalWorkingDays++;
+    let empCheck = Math. floor (Math. random() * 10)%3;
+    let empHrs = getWorkingHours(empCheck);
+    totalEmpHrs +=empHrs;
+    empDailyHrsAndWageArr.push({
+        dayNum:totalWorkingDays,
+        dailyHours : empHrs,
+        dailyWage :calcDailyWage(empHrs),
+        toString(){
+            return '\nDay' +this.dayNum + ' => Working Hours is '+ this.dailyHours
+            + 'And Wage Earned is ' + this.dailyWage
+        },
+    })
+}
+console.log("UC10 Showing Daily Hours Worked and Wage Earned " + empDailyHrsAndWageArr)
