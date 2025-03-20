a73faf9db5249e7f500683d5430c016139aa91a2
mahajandh <dhruvmahajan73@gmail.com>
2025-03-20 16:37:22 +0530

    UC5 code pushed

--- EmployeePresence.js
+++ EmployeePresence.js
@@ -54,3 +54,15 @@ for(let day=0; day<NUM_OF_WORKING_DAYS; day++){
 }
 empWage = empHrs * WAGE_PER_HOUR;
 console.log("Total Hrs: " + empHrs + "Emp Wage: "+ empWage);    
+// UC 5
+const MAX_HRS_IN_MONTH= 160;
+let totalEmpHrs =0;
+let totalWorkingDays =0;
+while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS){
+    totalWorkingDays++;
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    totalEmpHrs += getWorkingHours(empCheck) ;
+}
+empWage = totalEmpHrs * WAGE_PER_HOUR;
+console.log("UC5 — Total Days: "+ totalWorkingDays +" Total Hrs: "+ 
+    totalEmpHrs + " Emp Wage: "+ empWage) ;
